875d54c874d08f941a426412ae16d7d796b742e1
Maddog241 <1248826874@qq.com>
2024-11-25 04:37:12 +0000

    ADD: pass task0_1

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -33,6 +33,82 @@ from typing import Callable, Iterable
 
 
 # TODO: Implement for Task 0.1.
+def mul(a: float, b: float) -> float:
+    """Multiplies two numbers
+
+    Args:
+        a (float): number a 
+        b (float): number b 
+
+    Returns:
+        float: product of a and b
+        
+    """
+    return a * b
+
+def id(a: float) -> float:
+    """Return the input unchanged"""
+    return a
+
+def add(a: float, b: float) -> float:
+    """Add two numbers"""
+    return a + b
+
+def neg(a: float) -> float:
+    """Negates a number"""
+    return -a
+
+def lt(a: float, b: float) -> float:
+    """Checks if one number is less than another"""
+    return a < b
+
+def eq(a: float, b: float) -> float:
+    """Checks if two numbers are equal"""
+    return a == b
+
+def max(a: float, b: float) -> float:
+    """Returns the larger of two numbers"""
+    return a if a >= b else b
+
+def is_close(a: float, b: float) -> float:
+    """Checks if two numbers are close in value"""
+    return abs(a - b) < 1e-2
+
+def sigmoid(a: float) -> float:
+    """Calculates the sigmoid function"""
+    if a >= 0.0:
+        return 1.0 / (1.0 + math.exp(-a))
+    else:
+        return math.exp(a) / (1.0 + math.exp(a))
+
+def relu(a: float) -> float:
+    """Applies the ReLU activation"""
+    return a if a >= 0.0 else 0.0
+
+def log(a: float) -> float:
+    """Calculates the natural logarithm"""
+    return math.log(a)
+
+def exp(a: float) -> float:
+    """Calculates the exponential function"""
+    return math.exp(a)
+
+def inv(a: float) -> float:
+    """Calculates the reciprocal"""
+    return 1.0 / a
+
+def log_back(a: float, b: float) -> float:
+    """Computes the derivative of log times a seoncd arg"""
+    return inv(a) * b
+
+def inv_back(a: float, b: float) -> float:
+    """Computes the derivative of reciprocal times a second arg"""
+    return -1.0 / a**2 * b
+
+def relu_back(a: float, b: float) -> float:
+    """Computes the derivative of ReLU times a second arg"""
+    return b if a >= 0.0 else 0.0
+
 
 
 # ## Task 0.3
